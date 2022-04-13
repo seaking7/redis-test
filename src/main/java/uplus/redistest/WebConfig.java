c69d56309508a7143cc2024999edcc195983648a
Kim Taekyung <seaking7@gmail.com>
2022-04-13 22:13:02 +0900

    redis 패스워드 설정

--- src/main/java/uplus/redistest/WebConfig.java
+++ src/main/java/uplus/redistest/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,10 +19,17 @@ public class WebConfig implements WebMvcConfigurer {
     @Value("${spring.redis.port}")
     private int redisPort;
 
+    @Value("${spring.redis.password}")
+    private String redisPwd;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
         log.info("redis Server info {}, {}", redisHost, redisPort);
-        return new LettuceConnectionFactory(redisHost, redisPort);
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisHost);
+        redisStandaloneConfiguration.setPort(redisPort);
+        redisStandaloneConfiguration.setPassword(redisPwd);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
